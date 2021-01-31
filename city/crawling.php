aeae6074c6b75fe5effc9c4595a0700b8bc32aca
xiaoshangmin <shangminxiao@gmail.com>
2021-01-31 23:36:44 +0800

    update

--- city/crawling.php
+++ city/crawling.php
@@ -1,90 +1,112 @@
 <?php
-$url = 'http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2020/%s';
+const URL = 'http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2020/%s';
+
+function getOrMakePath(string $path)
+{
+    clearstatcache();
+    if (!is_dir($path)) {
+        mkdir($path, 0777, true);
+    }
+}
 
 function getProvice($url)
 {
     $pReg = "/<a href='((\d+)\.html)'>(\S+?)<br\/>/";
-    $url = sprintf($url, 'index.html');
-    $content = file_get_contents($url);
+    $proviceUrl = sprintf(URL, 'index.html');
+    $content = file_get_contents($proviceUrl);
     $content = iconv('gb2312', 'utf-8//IGNORE', $content);
     preg_match_all($pReg, $content, $match);
-    foreach ($match[1] as $url) {
-        echo $url;
+    $arr = [];
+    foreach ($match[1] as $index => $path) {
+        $city = getCity($path);
+        $arr[] = [
+            'code' => $match[2][$index],
+            'name' => $match[3][$index],
+            'path' => $path,
+            'city' => $city
+        ];
     }
-    foreach ($match[2] as $code) {
-        echo $code;
-    }
-    foreach ($match[3] as $name) {
-        echo $name;
-    }
+    print_r($arr);
 }
 
-function getCity($url, $path)
+getProvice($url);
+
+function getCity($path)
 {
     $cReg = "/<td><a href='([^']+)'>(\d+)<\/a><\/td><td><a href='[^']+'>(.*?)<\/a><\/td>/";
-    $url = sprintf($url, $path);
+    $cityUrl = sprintf(URL, $path);
+    $content = file_get_contents($cityUrl);
+    $content = iconv('gb2312', 'utf-8//IGNORE', $content);
+    preg_match_all($cReg, $content, $match);
+    $arr = [];
+    foreach ($match[1] as $index => $path) {
+        $city = getCounty($path);
+        $arr[] = [
+            'code' => $match[2][$index],
+            'name' => $match[3][$index],
+            'path' => $path,
+            'city' => $city
+        ];
+    }
+    return $arr;
+}
+
+
+function getCounty($path)
+{
+    $cReg = "/<td><a href='([^']+)'>(\d+)<\/a><\/td><td><a href='[^']+'>(.*?)<\/a><\/td>/";
+    $url = sprintf(URL, $path);
     $content = file_get_contents($url);
     $content = iconv('gb2312', 'utf-8//IGNORE', $content);
     preg_match_all($cReg, $content, $match);
-    foreach ($match[1] as $url) {
-        echo $url;
+    $arr = [];
+    foreach ($match[1] as $index => $path) {
+        $city = getTowntr($path);
+        $arr[] = [
+            'code' => $match[2][$index],
+            'name' => $match[3][$index],
+            'path' => $path,
+            'city' => $city
+        ];
     }
-    foreach ($match[2] as $code) {
-        echo $code;
-    }
-    foreach ($match[3] as $name) {
-        echo $name;
-    }
+    return $arr;
 }
 
-
-function getCounty($url, $path)
+function getTowntr($path)
 {
     $cReg = "/<td><a href='([^']+)'>(\d+)<\/a><\/td><td><a href='[^']+'>(.*?)<\/a><\/td>/";
-    $url = sprintf($url, $path);
+    $url = sprintf(URL, $path);
     $content = file_get_contents($url);
     $content = iconv('gb2312', 'utf-8//IGNORE', $content);
     preg_match_all($cReg, $content, $match);
-    foreach ($match[1] as $url) {
-        echo $url;
+    $arr = [];
+    foreach ($match[1] as $index => $path) {
+        $city = getVillagetr($path);
+        $arr[] = [
+            'code' => $match[2][$index],
+            'name' => $match[3][$index],
+            'path' => $path,
+            'city' => $city
+        ];
     }
-    foreach ($match[2] as $code) {
-        echo $code;
-    }
-    foreach ($match[3] as $name) {
-        echo $name;
-    }
+    print_r($arr);exit;
+    return $arr;
 }
 
-function getTowntr($url, $path)
-{
-    $cReg = "/<td><a href='([^']+)'>(\d+)<\/a><\/td><td><a href='[^']+'>(.*?)<\/a><\/td>/";
-    $url = sprintf($url, $path);
-    $content = file_get_contents($url);
-    $content = iconv('gb2312', 'utf-8//IGNORE', $content);
-    preg_match_all($cReg, $content, $match);
-    foreach ($match[1] as $url) {
-        echo $url;
-    }
-    foreach ($match[2] as $code) {
-        echo $code;
-    }
-    foreach ($match[3] as $name) {
-        echo $name;
-    }
-}
-
-function getVillagetr($url, $path)
+function getVillagetr($path)
 {
     $vReg = "/<tr[^>]+><td>(\d+)<\/td><td>[\d]+<\/td><td>(.*?)<\/td><\/tr>/";
-    $url = sprintf($url, $path);
+    $url = sprintf(URL, $path);
     $content = file_get_contents($url);
     $content = iconv('gb2312', 'utf-8//IGNORE', $content);
     preg_match_all($vReg, $content, $match);
-    foreach ($match[1] as $url) {
-        echo $url;
+    $arr = [];
+    foreach ($match[1] as $index => $path) {
+        $arr[] = [
+            'code' => $match[2][$index],
+            'name' => $match[3][$index],
+            'path' => $path,
+        ];
     }
-    foreach ($match[2] as $code) {
-        echo $code;
-    }
+    return $arr;
 }
