5eaedafc6d427c963628fb77f547fcd29162b319
root <shangminxiao@gmail.com>
2021-02-01 10:47:55 +0000

    update

--- city/crawling.php
+++ city/crawling.php
@@ -1,112 +1,127 @@
 <?php
-const URL = 'http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2020/%s';
 
-function getOrMakePath(string $path)
+class City
 {
-    clearstatcache();
-    if (!is_dir($path)) {
-        mkdir($path, 0777, true);
+
+    const URL = 'http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2020/%s';
+
+    public $code = '';
+
+    function getOrMakePath(string $path)
+    {
+        clearstatcache();
+        if (!is_dir($path)) {
+            mkdir($path, 0777, true);
+        }
+    }
+
+    //http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2020/index.html
+    function getProvice()
+    {
+        $pReg = "/<a href='((\d+)\.html)'>(\S+?)<br\/>/";
+        $url = sprintf(self::URL, 'index.html', '');
+        $content = file_get_contents($url);
+        $content = iconv('gb2312', 'utf-8//IGNORE', $content);
+        preg_match_all($pReg, $content, $match);
+        $arr = [];
+        foreach ($match[1] as $index => $path) {
+            $this->code = $match[2][$index];
+            $city = $this->getCity($path);
+            $arr[] = [
+                'code' => $match[2][$index],
+                'name' => $match[3][$index],
+                'path' => $path,
+                'city' => $city
+            ];
+        }
+        print_r($arr);
+    }
+
+    //http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2020/11.html
+    function getCity($path)
+    {
+        $cReg = "/<td><a href='([^']+)'>(\d+)<\/a><\/td><td><a href='[^']+'>(.*?)<\/a><\/td>/";
+        $url = sprintf(self::URL, $path, '');
+        $content = file_get_contents($url);
+        $content = iconv('gb2312', 'utf-8//IGNORE', $content);
+        preg_match_all($cReg, $content, $match);
+        $arr = [];
+        foreach ($match[1] as $index => $path) {
+            $city = $this->getCounty($path);
+            $arr[] = [
+                'code' => $match[2][$index],
+                'name' => $match[3][$index],
+                'path' => $path,
+                'city' => $city
+            ];
+        }
+        return $arr;
+    }
+
+
+    //http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2020/11/1101.html
+    function getCounty($path)
+    {
+        $cReg = "/<td><a href='([^']+)'>(\d+)<\/a><\/td><td><a href='[^']+'>(.*?)<\/a><\/td>/";
+        $url = sprintf(self::URL, $path);
+        $content = file_get_contents($url);
+        $content = iconv('gb2312', 'utf-8//IGNORE', $content);
+        preg_match_all($cReg, $content, $match);
+        $arr = [];
+        foreach ($match[1] as $index => $path) {
+            $city = $this->getTowntr($path);
+            $arr[] = [
+                'code' => $match[2][$index],
+                'name' => $match[3][$index],
+                'path' => $path,
+                'city' => $city
+            ];
+        }
+        return $arr;
+    }
+
+    //http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2020/11/01/110101.html
+    function getTowntr($path)
+    {
+        $cReg = "/<td><a href='([^']+)'>(\d+)<\/a><\/td><td><a href='[^']+'>(.*?)<\/a><\/td>/";
+        $path = $this->code . DIRECTORY_SEPARATOR . $path;
+        $url = sprintf(self::URL, $path);
+        $content = file_get_contents($url);
+        $content = iconv('gb2312', 'utf-8//IGNORE', $content);
+        preg_match_all($cReg, $content, $match);
+        $arr = [];
+        foreach ($match[1] as $index => $path) {
+            $city = $this->getVillagetr($path);
+            $arr[] = [
+                'code' => $match[2][$index],
+                'name' => $match[3][$index],
+                'path' => $path,
+                'city' => $city
+            ];
+        }
+        return $arr;
+    }
+
+    //http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2020/11/01/01/110101001.html
+    function getVillagetr($path)
+    {
+        $vReg = "/<tr[^>]+><td>(\d+)<\/td><td>[\d]+<\/td><td>(.*?)<\/td><\/tr>/";
+        $path = $this->code . '/01' . DIRECTORY_SEPARATOR . $path;
+        $url = sprintf(self::URL, $path);
+        $content = file_get_contents($url);
+        $content = iconv('gb2312', 'utf-8//IGNORE', $content);
+        preg_match_all($vReg, $content, $match); 
+        $arr = [];
+        foreach ($match[1] as $index => $path) {
+            $arr[] = [
+                'code' => $match[1][$index],
+                'name' => $match[2][$index],
+                'path' => $path,
+            ];
+        }
+        print_r($match);exit;
+        return $arr;
     }
 }
 
-function getProvice($url)
-{
-    $pReg = "/<a href='((\d+)\.html)'>(\S+?)<br\/>/";
-    $proviceUrl = sprintf(URL, 'index.html');
-    $content = file_get_contents($proviceUrl);
-    $content = iconv('gb2312', 'utf-8//IGNORE', $content);
-    preg_match_all($pReg, $content, $match);
-    $arr = [];
-    foreach ($match[1] as $index => $path) {
-        $city = getCity($path);
-        $arr[] = [
-            'code' => $match[2][$index],
-            'name' => $match[3][$index],
-            'path' => $path,
-            'city' => $city
-        ];
-    }
-    print_r($arr);
-}
-
-getProvice($url);
-
-function getCity($path)
-{
-    $cReg = "/<td><a href='([^']+)'>(\d+)<\/a><\/td><td><a href='[^']+'>(.*?)<\/a><\/td>/";
-    $cityUrl = sprintf(URL, $path);
-    $content = file_get_contents($cityUrl);
-    $content = iconv('gb2312', 'utf-8//IGNORE', $content);
-    preg_match_all($cReg, $content, $match);
-    $arr = [];
-    foreach ($match[1] as $index => $path) {
-        $city = getCounty($path);
-        $arr[] = [
-            'code' => $match[2][$index],
-            'name' => $match[3][$index],
-            'path' => $path,
-            'city' => $city
-        ];
-    }
-    return $arr;
-}
-
-
-function getCounty($path)
-{
-    $cReg = "/<td><a href='([^']+)'>(\d+)<\/a><\/td><td><a href='[^']+'>(.*?)<\/a><\/td>/";
-    $url = sprintf(URL, $path);
-    $content = file_get_contents($url);
-    $content = iconv('gb2312', 'utf-8//IGNORE', $content);
-    preg_match_all($cReg, $content, $match);
-    $arr = [];
-    foreach ($match[1] as $index => $path) {
-        $city = getTowntr($path);
-        $arr[] = [
-            'code' => $match[2][$index],
-            'name' => $match[3][$index],
-            'path' => $path,
-            'city' => $city
-        ];
-    }
-    return $arr;
-}
-
-function getTowntr($path)
-{
-    $cReg = "/<td><a href='([^']+)'>(\d+)<\/a><\/td><td><a href='[^']+'>(.*?)<\/a><\/td>/";
-    $url = sprintf(URL, $path);
-    $content = file_get_contents($url);
-    $content = iconv('gb2312', 'utf-8//IGNORE', $content);
-    preg_match_all($cReg, $content, $match);
-    $arr = [];
-    foreach ($match[1] as $index => $path) {
-        $city = getVillagetr($path);
-        $arr[] = [
-            'code' => $match[2][$index],
-            'name' => $match[3][$index],
-            'path' => $path,
-            'city' => $city
-        ];
-    }
-    print_r($arr);exit;
-    return $arr;
-}
-
-function getVillagetr($path)
-{
-    $vReg = "/<tr[^>]+><td>(\d+)<\/td><td>[\d]+<\/td><td>(.*?)<\/td><\/tr>/";
-    $url = sprintf(URL, $path);
-    $content = file_get_contents($url);
-    $content = iconv('gb2312', 'utf-8//IGNORE', $content);
-    preg_match_all($vReg, $content, $match);
-    $arr = [];
-    foreach ($match[1] as $index => $path) {
-        $arr[] = [
-            'code' => $match[2][$index],
-            'name' => $match[3][$index],
-            'path' => $path,
-        ];
-    }
-    return $arr;
-}
+(new City)->getProvice();
